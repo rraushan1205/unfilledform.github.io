ea40602f9ff071070543103d84e6cbd993bae8de
Raushan Raj <86942178+rraushan1205@users.noreply.github.com>
2023-05-04 16:11:53 +0530

    upload

---
+++ script.js
@@ -0,0 +1,177 @@
+$(function () {
+  // Window dimensions
+  var width = ($(window).width() - 100);
+  var height = ($(window).height() - 100);
+
+  // Show the image at location 100, 100
+  var p = $('#target').offset();
+  $('#target').offset(p);
+
+  // Click handler and image movement
+  $("#target").on('mouseover', function () {
+    x = Math.floor(Math.random() * width);
+    y = Math.floor(Math.random() * height);
+    var p = $('#target').offset();
+    p.top = y;
+    p.left = x;
+    $('#target').offset(p);
+  });
+});
+
+$(function () {
+  // Window dimensions
+  var width = ($(window).width() - 100);
+  var height = ($(window).height() - 100);
+
+  // Show the first image at location 10, 10
+  var p1 = $('#target1').offset();
+  $('#target1').offset(p1);
+
+  // Click handler and image movement for the first image
+  $("#target1").on('mouseover', function () {
+    x = Math.floor(Math.random() * width);
+    y = Math.floor(Math.random() * height);
+    var p1 = $('#target1').offset();
+    p1.top = y;
+    p1.left = x;
+    $('#target1').offset(p1);
+  });
+
+  // Show the second image at location 20, 20
+  var p2 = $('#target2').offset();
+  $('#target2').offset(p2);
+
+  // Click handler and image movement for the second image
+  $("#target2").on('mouseover', function () {
+    x = Math.floor(Math.random() * width);
+    y = Math.floor(Math.random() * height);
+    var p2 = $('#target2').offset();
+    p2.top = y;
+    p2.left = x;
+    $('#target2').offset(p2);
+  });
+
+  // Show the third image at location 30, 30
+  var p3 = $('#target3').offset();
+  $('#target3').offset(p3);
+
+  // Click handler and image movement for the third image
+  $("#target3").on('mouseover', function () {
+    x = Math.floor(Math.random() * width);
+    y = Math.floor(Math.random() * height);
+    var p3 = $('#target3').offset();
+    p3.top = y;
+    p3.left = x;
+    $('#target3').offset(p3);
+  });
+
+  // Show the fourth image at location 40, 40
+  var p4 = $('#target4').offset();
+  $('#target4').offset(p4);
+
+  // Click handler and image movement for the fourth image
+  $("#target4").on('mouseover', function () {
+    x = Math.floor(Math.random() * width);
+    y = Math.floor(Math.random() * height);
+    var p4 = $('#target4').offset();
+    p4.top = y;
+    p4.left = x;
+    $('#target4').offset(p4);
+  });
+
+  // Show the fifth image at location 50, 50
+  var p5 = $('#target5').offset();
+  $('#target5').offset(p5);
+
+  // Click handler and image movement for the fifth image
+  $("#target5").on('mouseover', function () {
+    x = Math.floor(Math.random() * width);
+    y = Math.floor(Math.random() * height);
+    var p5 = $('#target5').offset();
+    p5.top = y;
+    p5.left = x;
+    $('#target5').offset(p5);
+  });
+});
+
+$(function () {
+  // Window dimensions
+  var width = ($(window).width() - 100);
+  var height = ($(window).height() - 100);
+
+  // Show the image at location 100, 100
+  var q = $('#target6').offset();
+  $('#target6').offset(q);
+
+  // Click handler and image movement
+  $("#target6").on('mouseover', function () {
+    x = Math.floor(Math.random() * width);
+    y = Math.floor(Math.random() * height);
+    var q = $('#target6').offset();
+    q.top = y;
+    q.left = x;
+    $('#target6').offset(q);
+  });
+});
+
+
+$(function () {
+  // Window dimensions
+  var width = ($(window).width() - 100);
+  var height = ($(window).height() - 100);
+
+  // Show the image at location 100, 100
+  var r = $('#target7').offset();
+  $('#target7').offset(r);
+
+  // Click handler and image movement
+  $("#target7").on('mouseover', function () {
+    x = Math.floor(Math.random() * width);
+    y = Math.floor(Math.random() * height);
+    var r = $('#target7').offset();
+    r.top = y;
+    r.left = x;
+    $('#target7').offset(r);
+  });
+});
+
+
+$(function () {
+  // Window dimensions
+  var width = ($(window).width() - 100);
+  var height = ($(window).height() - 100);
+
+  // Show the image at location 100, 100
+  var s = $('#target8').offset();
+  $('#target8').offset(s);
+
+  // Click handler and image movement
+  $("#target8").on('mouseover', function () {
+    x = Math.floor(Math.random() * width);
+    y = Math.floor(Math.random() * height);
+    var s = $('#target8').offset();
+    s.top = y;
+    s.left = x;
+    $('#target8').offset(s);
+  });
+});
+
+$(function () {
+  // Window dimensions
+  var width = ($(window).width() - 100);
+  var height = ($(window).height() - 100);
+
+  // Show the image at location 100, 100
+  var g9 = $('#target9').offset();
+  $('#target9').offset(g9);
+
+  // Click handler and image movement
+  $("#target9").on('mouseover', function () {
+    x = Math.floor(Math.random() * width);
+    y = Math.floor(Math.random() * height);
+    var g9 = $('#target9').offset();
+    g9.top = y;
+    g9.left = x;
+    $('#target9').offset(g9);
+  });
+});
